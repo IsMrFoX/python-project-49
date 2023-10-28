04e3e31b13baa00297182097f555bcda145e913b
Rostislav <ylrostik@mail.ru>
2023-10-28 18:32:50 +0300

    expanded the work of the game with one number

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,12 @@
 from brain_games.cli import welcome_user
-from brain_games.games.even_or_odd import start_even_odd_game
+from brain_games.games.single_number_game import start_game
+from brain_games.game_logic import check_even_number
 
 
 def main():
     user_name = welcome_user()
-    start_even_odd_game(user_name)
+    game_greeting = 'Answer "yes" if the number is even, otherwise answer "no".'
+    start_game(user_name, game_greeting, check_even_number)
 
 
 if __name__ == "__main__":
