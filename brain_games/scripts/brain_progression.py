59e2a692639952dbe77e50084feec35820cc2fc3
Rostislav <ylrostik@mail.ru>
2023-10-28 19:32:31 +0300

    Fix

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,8 +1,10 @@
 from brain_games.cli import welcome_user
 from brain_games.games.progression import start_progression_game
+from brain_games.scripts.brain_games import main as greeting
 
 
 def main():
+    greeting()
     user_name = welcome_user()
     start_progression_game(user_name)
 
