d323ff87dd41cdd54124eb389b5724eb8bcc73bd
Rostislav <ylrostik@mail.ru>
2023-10-28 18:37:48 +0300

    Added new functionality for single-number games

---
+++ brain_games/games/single_number_game.py
@@ -0,0 +1,24 @@
+import prompt
+from random import randint
+from time import sleep
+
+
+def start_game(user_name, game_greeting, check_func):
+    num_of_questions = 3
+    print(game_greeting)
+    while num_of_questions:
+        number = randint(0, 1000)
+        answer = prompt.string(f'Question: {number}  ').lower()
+        print(f'Your answer: {answer} ')
+        is_correct, message = check_func(number, answer)
+        if not is_correct:
+            print(message)
+            print(f"Let's try again, {user_name}")
+            break
+        else:
+            print(message)
+            num_of_questions -= 1
+            sleep(0.5)
+
+    if not num_of_questions:
+        print(f'Congratulations, {user_name}!')
