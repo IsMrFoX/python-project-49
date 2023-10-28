cf23b27494754bb4bddd76cc8bab0f42c6b96108
Rostislav <ylrostik@mail.ru>
2023-10-28 19:33:12 +0300

    Fix

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,8 +1,10 @@
 from brain_games.cli import welcome_user
 from brain_games.games.calc import start_calc_game
+from brain_games.scripts.brain_games import main as greeting
 
 
 def main():
+    greeting()
     user_name = welcome_user()
     start_calc_game(user_name)
 
