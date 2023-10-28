1b131454ee15ee4d80b7f94d71fb30d3cb8c1498
Rostislav <ylrostik@mail.ru>
2023-10-28 15:16:34 +0300

    Added new game logik

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,3 +1,6 @@
+import math
+
+
 def check_even_number(number, answer):
     if number % 2 and answer == 'yes':
         return False, f"'{answer}' is wrong answer ;(. Correct answer was 'no'."
@@ -15,3 +18,11 @@ def check_expression(f_num, s_num, oper, answer):
 ;(. Correct answer was '{expression}'."
     else:
         return True, 'Correct!'
+
+
+def check_gcd_number(num1, num2, answer):
+    gcd_result = math.gcd(num1, num2)
+    if gcd_result == int(answer):
+        return True, 'correct!'
+    return False, f"'{answer}' is wrong answer \
+;(. Correct answer was '{gcd_result}'."
